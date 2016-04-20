ee32fc65691a6989ddb576b71386d77559b354d6
derekscode <derekscode@github.com>
2016-04-20 14:27:15 -0700

    finished Building Nested Components

--- app/products/product-list.component.ts
+++ app/products/product-list.component.ts
@@ -1,12 +1,18 @@
-﻿import { Component } from 'angular2/core'
+﻿import { Component, OnInit } from 'angular2/core'
+
 import {IProduct} from './product'
+import {ProductFilterPipe} from './product-filter.pipe'
+import {StarComponent} from '../shared/star.component'
 
 @Component({
     selector: 'pm-products',
-    templateUrl: 'app/products/product-list.component.html'
+    templateUrl: 'app/products/product-list.component.html',
+    styleUrls: ['app/products/product-list.component.css'],
+    pipes: [ProductFilterPipe],
+    directives: [StarComponent]
 })
 
-export class ProductListComponent {
+export class ProductListComponent implements OnInit{
     pageTitle: string = 'Product List';
     imageWidth: number = 50;
     imageMargin: number = 2;
@@ -68,6 +74,14 @@ export class ProductListComponent {
     
     toggleImage(): void {
         this.showImage = !this.showImage;
+    }
+    
+    ngOnInit(): void{
+        console.log('In OnInit');
+    }
+    
+    onRatingClicked(message: string): void{
+        this.pageTitle = "Product List: " + message;
     }
 
 
