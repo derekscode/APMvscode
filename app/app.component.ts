8c8d7317593af267361935dabdfb07e2fd3e3e75
derekscode <derekscode@github.com>
2016-04-20 18:11:58 -0700

    finished Retrieving Data Using Http

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,11 +1,15 @@
-﻿import {Component} from 'angular2/core';
+﻿import {Component} from 'angular2/core'
+import { HTTP_PROVIDERS } from 'angular2/http'
+import 'rxjs/Rx';   // Load all features
 
 import {ProductListComponent} from './products/product-list.component'
+import {ProductService} from './products/product.service'
 
 @Component({
     selector: 'pm-app',
     templateUrl: 'app/views/index.html',
-    directives: [ProductListComponent]
+    directives: [ProductListComponent],
+    providers: [ProductService, HTTP_PROVIDERS]
 })
 export class AppComponent {
     pageTitle: string = 'Acme Product Management';
