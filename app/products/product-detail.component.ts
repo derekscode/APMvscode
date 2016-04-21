3990cc31a8ddb3dd2e21e57e90ec4d6e4c45bbfc
derekscode <derekscode@github.com>
2016-04-21 14:09:14 -0700

    finished

---
+++ app/products/product-detail.component.ts
@@ -0,0 +1,44 @@
+import { Component } from 'angular2/core'
+import { RouteParams, Router } from 'angular2/router'
+
+import { IProduct } from './product';
+import { ProductService } from './product.service';
+import { StarComponent } from '../shared/star.component';
+
+
+@Component({
+    templateUrl: 'app/products/product-detail.component.html',
+    directives: [StarComponent]
+})
+
+export class ProductDetailComponent {
+    pageTitle: string = 'Product Detail';
+    product: IProduct;
+    errorMessage: string;
+
+
+    constructor(private _productService: ProductService,
+        private _router: Router,
+        private _routeParams: RouteParams) {
+    }
+
+    ngOnInit() {
+        if (!this.product) {
+            let id = +this._routeParams.get('id');
+            // this.pageTitle += `: ${id}`;
+            this.getProduct(id);
+        }
+    }
+
+    getProduct(id: number) {
+        this._productService.getProduct(id)
+            .subscribe(
+            product => this.product = product,
+            error => this.errorMessage = <any>error);
+    }
+
+
+    onBack(): void {
+        this._router.navigate(['Products']);
+    }
+}
